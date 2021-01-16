82bd3ec289564b01f6d96fbb57856dc3f46b8727
jtaylor06 <josiahtaylor2020@gmail.com>
2021-01-16 13:00:23 -0500

    Added in swiping and temp button to navigate to swipe screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print('in MyApp');
+    
     return MaterialApp(
         initialRoute: '/',
         routes: {
